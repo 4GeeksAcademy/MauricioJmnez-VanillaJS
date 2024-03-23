206a5bc374f576de9ae1e8b3d36884c751b1a1d2
Mauricio Jiménez <162180771+MauricioJmnez@users.noreply.github.com>
2024-03-23 02:18:17 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#botón").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generarExcusa();
+  });
+  console.log("This is the final project!");
+};
+
+let generarExcusa = () => {
+  let who = ["The dog", "My little brother", "The cat", "A racoon"];
+  let action = ["bit", "stole", "puked on", "broke"];
+  let what = [
+    "my homework",
+    "the internet modem",
+    "my phone",
+    "the birthday cake"
+  ];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was showering"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+  //No sé por qué la última línea de código editada siempre se divide//
 };
